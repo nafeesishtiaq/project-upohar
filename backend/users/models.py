2d91515cb4e7faacf5218874a733dbb3551fb684
nafeesishtiaq <nafeesishtiaq1@gmail.com>
2026-01-20 00:18:35 +0600

    register updated

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,21 +3,26 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+      extra_fields.setdefault('is_staff', True)
+      extra_fields.setdefault('is_superuser', True)
+      extra_fields.setdefault("is_active", True)
+
+      if extra_fields.get("is_staff") is not True:
+        raise ValueError("Superuser must have is_staff=True")
+      if extra_fields.get("is_superuser") is not True:
+        raise ValueError("Superuser must have is_superuser=True")
+
+      return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -27,6 +32,6 @@ class User(AbstractUser):
   REQUIRED_FIELDS = [] 
   
   objects = UserManager()
-  
+
   def __str__(self):
     return self.email
